f904b20c8ce888ae1aafd2deca11cc0f411cb127
MonaAbolghasemi <113474895+MonaAbolghasemi@users.noreply.github.com>
2022-11-23 01:40:45 +0100

    Update sketch.js
    Children Drawing app

--- sketch.js
+++ sketch.js
@@ -1,16 +1,69 @@
+
+
+
+//image preload
+let myImage;
+var colors;
+var myImages;
+
 function preload() {
-	// put preload code here
+  myImage1 = loadImage("./assets/01.jpg");
+  myImage2 = loadImage("./assets/02.jpg");
+  myImage3 = loadImage("./assets/03.jpg");
+  myImage4 = loadImage("./assets/04.jpg");
+  myImage5 = loadImage("./assets/05.jpg");
+  myImage6 = loadImage("./assets/06.jpg");
+  myImage7 = loadImage("./assets/07.jpg");
+  
 }
 
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  // Make the canvas the size of the mobile device screen
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  // An array of 7 image
+  myImages = [myImage1, myImage2, myImage3, myImage4, myImage5, myImage6, myImage7,]
+  myImage = random(myImages)
+  image(myImage, 0, 0, windowWidth, windowHeight);
+  
+  button = createButton('Change the maze');
+  button.position(width/3 - width/6, height/12);
+  button.size(width/3.2,height/15);
+
+  buttonClear = createButton('Clear');
+  buttonClear.position(3*(width/3 - width/6), height/12);
+  buttonClear.size(width/3,height/15);
+}
+// A function for changing the image of maze randomly by pressing the button
+function changeImage() {
+ 
+  let newImage;
+  newImage = random(myImages)
+  while(newImage == myImage) {
+    newImage = random(myImages);
+  }
+  myImage = newImage
+  image(myImage, 0, 0, windowWidth, windowHeight);
+}
+
+function clearImage() {
+image(myImage, 0, 0, windowWidth, windowHeight);
 }
 
 function draw() {
-	// put drawing code here
+  button.mousePressed(changeImage);
+  buttonClear.mousePressed(clearImage)
+  for (var i = 0; i < touches.length; i++) {
+    noStroke();
+    // One color per finger
+    fill(255,0,0);
+    // Draw a circle at each finger
+    ellipse(touches[i].x, touches[i].y, 5, 5);
+  }
+}
+
+// this prevents dragging screen around
+function touchMoved() {
+  return false;
 }
